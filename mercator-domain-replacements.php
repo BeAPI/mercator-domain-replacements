8e2832b6f5ad0263729010ae2856a1c314fcdd20
Nicolas JUEN <njuen@beapi.fr>
2023-11-22 10:29:52 +0100

    fix: Do replace the parent theme url too when resource is on parent and not child.

--- mercator-domain-replacements.php
+++ mercator-domain-replacements.php
@@ -105,6 +105,10 @@ class Mapping {
 			$theme_default_path                   = get_theme_file_uri();
 			$this->domains[ $theme_default_path ] = str_replace( $network_domain_internal, $domain_mapped, $theme_default_path );
 
+			// Change parent theme path to the mapped theme path
+			$theme_parent_path                   = get_parent_theme_file_uri();
+			$this->domains[ $theme_parent_path ] = str_replace( $network_domain_internal, $domain_mapped, $theme_parent_path );
+
 			// Change DNS Prefetch path
 			// Use "href" to replace only network URLs starting with "//" and not all network URLs because there may be network URLs contributing.
 			$this->domains[ "href='//" . wp_parse_url( $network_domain_internal, PHP_URL_HOST ) ] = "href='//" . wp_parse_url( $domain_mapped, PHP_URL_HOST );
